db949f28c324b07dc511c67e95b3a5f316f83ce2
Mark Umnus <merkrafter@gmail.com>
2019-11-18 23:08:23 +0100

    Add test case for not specifying --output

--- src/test/java/com/merkrafter/MerkompilerRunTest.java
+++ src/test/java/com/merkrafter/MerkompilerRunTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -65,6 +67,44 @@ class MerkompilerRunTest {
     }
 
     /**
+     * This test case runs the lexer on the file(s) given by ValueSource.
+     * It assumes that there are <code>baseFileName</code>{@value INPUT_FILE_SUFFIX} and
+     * <code>baseFileName</code>{@value EXPECTED_FILE_SUFFIX} present under
+     * <code>src/test/resources</code> in the project.
+     * The output for this experiment is not specified, hence it tests writing to stdout.
+     * <p>
+     * This method resets System.out in order to test the output written to it.
+     * If this method throws an exception, System.out might still be unavailable.
+     *
+     * @param baseFileName if used to find the source file name and expected file name
+     * @throws ArgumentParserException if the arguments in the test case are misconfigured (should not happen)
+     * @throws IOException if there is a read/write error in one of the files
+     */
+    @ParameterizedTest
+    @ValueSource(strings = "EmptyClass")
+    void runWithoutOutput(final String baseFileName) throws ArgumentParserException, IOException {
+        final PrintStream originalOut = System.out;
+        try { // will reset System.out in case of errors
+            final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+            // java source file to read
+            final File inputFile = getFileFromResource(baseFileName + INPUT_FILE_SUFFIX);
+            // file that should identical content as output after run method
+            final File expectedFile = getFileFromResource(baseFileName + EXPECTED_FILE_SUFFIX);
+            // set stdout to testable output stream
+            System.setOut(new PrintStream(output));
+
+            // run main program without specifying output
+            final Config config = Config.fromArgs(inputFile.getAbsolutePath());
+            Merkompiler.run(config);
+
+            assertEquals(toString(expectedFile), output.toString().trim());
+        } finally {
+            System.setOut(originalOut); // reset System.out even in case of errors
+        }
+    }
+
+    /**
      * Reads the given file using this class's class loader, checks for its existence and finally
      * returns it.
      *
@@ -92,5 +132,17 @@ class MerkompilerRunTest {
     throws IOException {
         assertEquals(Files.readAllLines(expectedFile.toPath()),
                      Files.readAllLines(actualFile.toPath()));
+    }
+
+    /**
+     * Reads the file contents and joins them to a single string.
+     *
+     * @param file the file to read
+     * @return a string containing the lines joined with a newline character
+     *
+     * @throws IOException if a read/write error occurs
+     */
+    private static String toString(final File file) throws IOException {
+        return String.join("\n", Files.readAllLines(file.toPath()));
     }
 }
