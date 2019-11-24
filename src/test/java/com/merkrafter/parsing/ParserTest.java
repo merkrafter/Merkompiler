bb69777330e2b05011792203775dd2216db05a74
Mark Umnus <merkrafter@gmail.com>
2019-11-24 16:26:52 +0100

    Add happy path test cases for parser methods

--- src/test/java/com/merkrafter/parsing/ParserTest.java
+++ src/test/java/com/merkrafter/parsing/ParserTest.java
@@ -14,6 +14,149 @@ import static org.junit.jupiter.api.Assertions.*;
 class ParserTest {
 
     /**
+     * The parser should accept a single pair of parentheses as actual parameters.
+     */
+    @Test
+    void parseEmptyActualParameters() {
+        final Scanner scanner = new TestScanner(new Token[]{
+                new Token(TokenType.L_PAREN, "", 0, 0), new Token(TokenType.R_PAREN, "", 0, 0)});
+        final Parser parser = new Parser(scanner);
+        assertTrue(parser.parseActualParameters());
+    }
+
+    /**
+     * The parser should accept a single comparison between an ident and a number as an expression.
+     */
+    @ParameterizedTest
+    @EnumSource(value = TokenType.class, names = {
+            "LOWER", "LOWER_EQUAL", "EQUAL", "GREATER_EQUAL", "GREATER"})
+    void parseSingleComparisonAsExpression(final TokenType comparisonType) {
+        final Scanner scanner = new TestScanner(new Token[]{
+                new Token(TokenType.IDENT, "", 0, 0),
+                new Token(comparisonType, "", 0, 0),
+                new Token(TokenType.NUMBER, "", 0, 0)});
+        final Parser parser = new Parser(scanner);
+        assertTrue(parser.parseExpression());
+    }
+
+    /**
+     * The parser should accept a single addition/subtraction as a simple expression.
+     */
+    @ParameterizedTest
+    @EnumSource(value = TokenType.class, names = {"PLUS", "MINUS"})
+    void parseSimpleExpression(final TokenType tokenType) {
+        final Scanner scanner = new TestScanner(new Token[]{
+                new Token(TokenType.IDENT, "", 0, 0),
+                new Token(tokenType, "", 0, 0),
+                new Token(TokenType.NUMBER, "", 0, 0)});
+        final Parser parser = new Parser(scanner);
+        assertTrue(parser.parseSimpleExpression());
+    }
+
+    /**
+     * The parser should accept a single multiplication/division as a term.
+     */
+    @ParameterizedTest
+    @EnumSource(value = TokenType.class, names = {"TIMES", "DIVIDE"})
+    void parseTerm(final TokenType tokenType) {
+        final Scanner scanner = new TestScanner(new Token[]{
+                new Token(TokenType.IDENT, "", 0, 0),
+                new Token(tokenType, "", 0, 0),
+                new Token(TokenType.NUMBER, "", 0, 0)});
+        final Parser parser = new Parser(scanner);
+        assertTrue(parser.parseTerm());
+    }
+
+    /**
+     * The parser should accept an intern procedure call without arguments as a factor.
+     */
+    @Test
+    void parseInternProcedureCallWithoutArgsAsFactor() {
+        final Scanner scanner = new TestScanner(new Token[]{
+                new Token(TokenType.IDENT, "", 0, 0),
+                new Token(TokenType.L_PAREN, "", 0, 0),
+                new Token(TokenType.R_PAREN, "", 0, 0)});
+        final Parser parser = new Parser(scanner);
+        assertTrue(parser.parseFactor());
+    }
+
+    /**
+     * The parser should accept an intern procedure call with only one argument (number or ident)
+     * as a factor.
+     */
+    @ParameterizedTest
+    @EnumSource(value = TokenType.class, names = {"NUMBER", "IDENT"})
+    void parseInternProcedureCallWithOneArgAsFactor(final TokenType tokenType) {
+        final Scanner scanner = new TestScanner(new Token[]{
+                new Token(TokenType.IDENT, "", 0, 0),
+                new Token(TokenType.L_PAREN, "", 0, 0),
+                new Token(tokenType, "", 0, 0),
+                new Token(TokenType.R_PAREN, "", 0, 0)});
+        final Parser parser = new Parser(scanner);
+        assertTrue(parser.parseFactor());
+    }
+
+    /**
+     * The parser should accept an intern procedure call with two comma-separated arguments (number
+     * and an identifier) as a factor.
+     */
+    @Test
+    void parseInternProcedureCallWithTwoArgsAsFactor() {
+        final Scanner scanner = new TestScanner(new Token[]{
+                new Token(TokenType.IDENT, "", 0, 0),
+                new Token(TokenType.L_PAREN, "", 0, 0),
+                new Token(TokenType.NUMBER, "", 0, 0),
+                new Token(TokenType.COMMA, "", 0, 0),
+                new Token(TokenType.IDENT, "", 0, 0),
+                new Token(TokenType.R_PAREN, "", 0, 0)});
+        final Parser parser = new Parser(scanner);
+        assertTrue(parser.parseFactor());
+    }
+
+    /**
+     * The parser should accept a simple expression representing a binary operation between a
+     * number and an identifier as a factor.
+     */
+    @ParameterizedTest
+    @EnumSource(value = TokenType.class, names = {"PLUS", "MINUS", "TIMES", "DIVIDE"})
+    void parseBinOpExpressionAsFactor(final TokenType binOp) {
+        final Scanner scanner = new TestScanner(new Token[]{
+                new Token(TokenType.L_PAREN, "", 0, 0),
+                new Token(TokenType.NUMBER, "", 0, 0),
+                new Token(binOp, "", 0, 0),
+                new Token(TokenType.IDENT, "", 0, 0),
+                new Token(TokenType.R_PAREN, "", 0, 0)});
+        final Parser parser = new Parser(scanner);
+        assertTrue(parser.parseFactor());
+    }
+
+    /**
+     * The parser should accept a simple expression (consisting only of parentheses and
+     * identifier/number in between) as a factor.
+     */
+    @ParameterizedTest
+    @EnumSource(value = TokenType.class, names = {"IDENT", "NUMBER"})
+    void parseSimpleExpressionAsFactor(final TokenType tokenType) {
+        final Scanner scanner = new TestScanner(new Token[]{
+                new Token(TokenType.L_PAREN, "", 0, 0),
+                new Token(tokenType, "", 0, 0),
+                new Token(TokenType.R_PAREN, "", 0, 0)});
+        final Parser parser = new Parser(scanner);
+        assertTrue(parser.parseFactor());
+    }
+
+    /**
+     * The parser should accept a single identifier or number as a factor.
+     */
+    @ParameterizedTest
+    @EnumSource(value = TokenType.class, names = {"IDENT", "NUMBER"})
+    void parseIdentifierOrNumberAsFactor(final TokenType tokenType) {
+        final Scanner scanner = new TestScanner(new Token[]{new Token(tokenType, "", 0, 0)});
+        final Parser parser = new Parser(scanner);
+        assertTrue(parser.parseFactor());
+    }
+
+    /**
      * The parser should accept a single number.
      */
     @Test
