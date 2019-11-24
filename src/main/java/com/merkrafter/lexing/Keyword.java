aea8126ab01f4959ae2931430646cea18ff503e7
Mark Umnus <merkrafter@gmail.com>
2019-11-24 20:01:08 +0100

    Add Keywords enum

---
+++ src/main/java/com/merkrafter/lexing/Keyword.java
@@ -0,0 +1,19 @@
+package com.merkrafter.lexing;
+
+/****
+ * This enum lists all keywords that can be encountered in JavaSST files.
+ *
+ * @since v0.2.0
+ * @author merkrafter
+ ***************************************************************/
+public enum Keyword {
+    CLASS,
+    ELSE,
+    FINAL,
+    IF,
+    INT,
+    PUBLIC,
+    RETURN,
+    VOID,
+    WHILE,
+}
