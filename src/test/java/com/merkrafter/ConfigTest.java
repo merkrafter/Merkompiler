acd9007bcd4853711a64af45d5023f08c9bd2c5c
Mark Umnus <merkrafter@gmail.com>
2019-11-30 23:18:55 +0100

    Add comments to ConfigTest cases

--- src/test/java/com/merkrafter/ConfigTest.java
+++ src/test/java/com/merkrafter/ConfigTest.java
@@ -10,8 +10,19 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static com.merkrafter.config.Config.fromString;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * The test cases of this class verify that the conversion from command line arguments to program
+ * configuration object works correctly. Therefore, the Config::fromArgs static method is tested
+ * intensely.
+ */
 class ConfigTest {
 
+    /**
+     * The fromArgs method should be able to extract the input file name correctly.
+     * It should not set the verbosity nor the output file name.
+     *
+     * @throws ArgumentParserException if the arguments can not be parsed; should not happen
+     */
     @Test
     void parseOnlyInputFile() throws ArgumentParserException {
         final String[] args = fromString("Test.java");
@@ -26,6 +37,13 @@ class ConfigTest {
         assertEquals(expectedVerbosity, actualConfig.isVerbose());
     }
 
+    /**
+     * The fromArgs method should be able to detect the verbosity flag being set, independent of
+     * whether the long or short argument was used or whether it was specified before or after
+     * the input file.
+     *
+     * @throws ArgumentParserException if the arguments can not be parsed; should not happen
+     */
     @ParameterizedTest
     // {short, long} x {before input file, after input file}
     @ValueSource(strings = {
@@ -41,6 +59,13 @@ class ConfigTest {
         assertEquals(expectedVerbosity, actualConfig.isVerbose());
     }
 
+    /**
+     * The fromArgs method should be able to detect the output file being specified, independent of
+     * whether the long or short argument was used, it was assigned using = or not or whether
+     * it was specified before or after the input file.
+     *
+     * @throws ArgumentParserException if the arguments can not be parsed; should not happen
+     */
     @ParameterizedTest
     // {short, long} x {before input file, after input file}
     @ValueSource(strings = {
@@ -61,6 +86,12 @@ class ConfigTest {
         assertEquals(expectedOutputFilename, actualConfig.getOutputFile());
     }
 
+    /**
+     * The fromArgs method should be able to extract the output file and the verbosity flag if both
+     * are given at the same time.
+     *
+     * @throws ArgumentParserException if the arguments can not be parsed; should not happen
+     */
     @Test
     void parseInputFileAndOutputFileWithVerbosity() throws ArgumentParserException {
         final String[] args = fromString("-v -o OtherTest.class Test.java");
@@ -75,6 +106,12 @@ class ConfigTest {
         assertEquals(expectedVerbosity, actualConfig.isVerbose());
     }
 
+    /**
+     * The fromArgs method should be able to extract the compiler stage "scanning" while ignoring
+     * the case.
+     *
+     * @throws ArgumentParserException if the arguments can not be parsed; should not happen
+     */
     @ParameterizedTest
     @ValueSource(strings = {"scanning", "SCANNING", "sCanning", "sCaNnInG"})
     void skipAfterScanning(final String spelling) throws ArgumentParserException {
@@ -86,6 +123,12 @@ class ConfigTest {
         assertEquals(expectedStage, actualConfig.getStage());
     }
 
+    /**
+     * The fromArgs method should be able to extract the compiler stage "parsing" while ignoring
+     * the case.
+     *
+     * @throws ArgumentParserException if the arguments can not be parsed; should not happen
+     */
     @ParameterizedTest
     @ValueSource(strings = {"parsing", "PARSING", "pArsing", "pArSinG"})
     void skipAfterParsing(final String spelling) throws ArgumentParserException {
@@ -97,6 +140,12 @@ class ConfigTest {
         assertEquals(expectedStage, actualConfig.getStage());
     }
 
+    /**
+     * The fromArgs method should set the latest compiler stage correctly when it is not specified
+     * explicitly.
+     *
+     * @throws ArgumentParserException if the arguments can not be parsed; should not happen
+     */
     @Test
     void defaultStage() throws ArgumentParserException {
         final String[] args = fromString("Test.java");
