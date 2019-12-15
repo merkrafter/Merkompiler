589b0433ee1a3a1b8379d7f90b4e22159b20005e
Mark Umnus <merkrafter@gmail.com>
2019-12-15 21:54:47 +0100

    Add Type enum

---
+++ src/main/java/com/merkrafter/representation/Type.java
@@ -0,0 +1,12 @@
+package com.merkrafter.representation;
+
+/****
+ * This class stores information on a type in JavaSST.
+ *
+ * @since v0.3.0
+ * @author merkrafter
+ ***************************************************************/
+public enum Type {
+    INT,
+    VOID;
+}
