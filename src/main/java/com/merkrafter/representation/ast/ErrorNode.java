50c8a42929d1810ee01b35a8706e3c8a144882d6
Mark Umnus <merkrafter@gmail.com>
2020-01-10 22:18:26 +0100

    Implement ErrorNode

---
+++ src/main/java/com/merkrafter/representation/ast/ErrorNode.java
@@ -0,0 +1,47 @@
+package com.merkrafter.representation.ast;
+
+import com.merkrafter.representation.Type;
+
+/****
+ * This node class signals an error (a syntax or type error, for instance).
+ *
+ * @since v0.3.0
+ * @author merkrafter
+ ***************************************************************/
+public class ErrorNode extends ASTBaseNode {
+    // ATTRIBUTES
+    //==============================================================
+    private final String message;
+
+    // CONSTRUCTORS
+    //==============================================================
+
+    /****
+     * Creates a new ErrorNode with a given error message.
+     ***************************************************************/
+    public ErrorNode(final String message) {
+        this.message = message;
+    }
+
+    // GETTER
+    //==============================================================
+
+    /**
+     * Returns the error message.
+     *
+     * @return the error message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Errors do not have any types hence this method always returns Type.VOID.
+     *
+     * @return Type.VOID
+     */
+    @Override
+    Type getReturnedType() {
+        return Type.VOID;
+    }
+}
