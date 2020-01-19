3e220a73ad4dfdc797f6f8e97f32995ac7fae521
Mark Umnus <merkrafter@gmail.com>
2020-01-19 17:24:03 +0100

    Implement ClassDescription

---
+++ src/main/java/com/merkrafter/representation/ClassDescription.java
@@ -0,0 +1,54 @@
+package com.merkrafter.representation;
+
+import com.merkrafter.representation.ast.ProcedureCallNode;
+
+/****
+ * This class stores information on a JavaSST class.
+ *
+ * @since v0.3.0
+ * @author merkrafter
+ ***************************************************************/
+public class ClassDescription extends ObjectDescription {
+    // ATTRIBUTES
+    //==============================================================
+    private final SymbolTable symbolTable;
+    private ProcedureCallNode entryPoint;
+
+    // CONSTRUCTORS
+    //==============================================================
+
+    /****
+     * Creates a new ClassDescription from a name and with a symbol table from the outer scope.
+     ***************************************************************/
+    public ClassDescription(final String name, final SymbolTable outerScope) {
+        super(name);
+        symbolTable = new SymbolTable(outerScope);
+    }
+
+    // GETTER
+    //==============================================================
+
+    /**
+     * @return table of all variables and methods that are defined in this class
+     */
+    public SymbolTable getSymbolTable() {
+        return symbolTable;
+    }
+
+    /**
+     * @return the entry point of this class
+     */
+    public ProcedureCallNode getEntryPoint() {
+        return entryPoint;
+    }
+
+    // SETTER
+    //==============================================================
+
+    /**
+     * @param entryPoint a procedure that will be called when the program starts
+     */
+    public void setEntryPoint(final ProcedureCallNode entryPoint) {
+        this.entryPoint = entryPoint;
+    }
+}
