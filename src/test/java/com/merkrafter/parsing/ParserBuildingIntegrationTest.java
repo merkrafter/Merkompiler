42ab8ec4b833ea73fc74f13281f9f9474e41f531
Mark Umnus <merkrafter@gmail.com>
2020-01-20 23:52:02 +0100

    Create empty integration test class for the AST creation

---
+++ src/test/java/com/merkrafter/parsing/ParserBuildingIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.merkrafter.parsing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This class contains some small(er) test cases that aim for edge cases and concrete scenarios.
+ * They are tested together with the scanner which enables the source code to be much easier
+ * readable.
+ */
+class ParserBuildingIntegrationTest {
+
+}
