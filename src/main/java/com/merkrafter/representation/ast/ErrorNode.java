6b708146b04fe90f584e9ce20f28be51bfca4c31
Mark Umnus <merkrafter@gmail.com>
2020-01-20 21:57:42 +0100

    Make ErrorNode implement Expression and Statement

--- src/main/java/com/merkrafter/representation/ast/ErrorNode.java
+++ src/main/java/com/merkrafter/representation/ast/ErrorNode.java
@@ -7,11 +7,14 @@ import java.util.List;
 
 /****
  * This node class signals an error (a syntax or type error, for instance).
+ * <p>
+ * Implementing the Expression and Statement interfaces is more like a hack to allow
+ * this node to be returned by the parser methods.
  *
  * @since v0.3.0
  * @author merkrafter
  ***************************************************************/
-public class ErrorNode extends ASTBaseNode {
+public class ErrorNode implements Expression, Statement {
     // ATTRIBUTES
     //==============================================================
     private final String message;
@@ -39,7 +42,8 @@ public class ErrorNode extends ASTBaseNode {
     }
 
     /**
-     * Errors do not have any types hence this method always returns Type.VOID.
+     * This method is only here to fulfill the requirements for implementing Statement. It does not
+     * have any meaningful implementation.
      *
      * @return Type.VOID
      */
@@ -76,5 +80,26 @@ public class ErrorNode extends ASTBaseNode {
         final List<String> errors = new LinkedList<>();
         errors.add(message);
         return errors;
+    }
+
+    /**
+     * This method is only here to fulfill the requirements for implementing Statement. It does not
+     * have any meaningful implementation.
+     *
+     * @return null
+     */
+    @Override
+    public Statement getNext() {
+        return null;
+    }
+
+    /**
+     * This method is only here to fulfill the requirements for implementing Statement. It does not
+     * have any meaningful implementation.
+     *
+     * @param next does not matter
+     */
+    @Override
+    public void setNext(final Statement next) {
     }
 }
