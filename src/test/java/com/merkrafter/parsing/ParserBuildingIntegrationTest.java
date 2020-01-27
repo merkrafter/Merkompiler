8b759ab1dcfe1a79d8a77c110ba938468d4889aa
Mark Umnus <merkrafter@gmail.com>
2020-01-27 19:06:08 +0100

    Add test case for the operator precedence of multiplication over addition

--- src/test/java/com/merkrafter/parsing/ParserBuildingIntegrationTest.java
+++ src/test/java/com/merkrafter/parsing/ParserBuildingIntegrationTest.java
@@ -2,9 +2,15 @@ package com.merkrafter.parsing;
 
 import com.merkrafter.lexing.Scanner;
 import com.merkrafter.lexing.StringIteratorTestUtility;
+import com.merkrafter.representation.ast.ASTBaseNode;
+import com.merkrafter.representation.ast.BinaryOperationNode;
+import com.merkrafter.representation.ast.BinaryOperationNodeType;
+import com.merkrafter.representation.ast.ConstantNode;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.merkrafter.representation.Type.INT;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * This class contains some small(er) test cases that aim for edge cases and concrete scenarios.
@@ -30,5 +36,26 @@ class ParserBuildingIntegrationTest {
         stringIterator = new StringIteratorTestUtility();
         scanner = new Scanner(stringIterator);
         parser = new Parser(scanner);
+    }
+
+    /**
+     * The generated AST should reflect the operator precedence of multiplication over addition.
+     */
+    @Test
+    void multiplicationBeforeAddition() {
+        final String program = "5+3*8";
+        stringIterator.setString(program);
+        scanner = new Scanner(stringIterator);
+        parser = new Parser(scanner);
+        final ASTBaseNode multTree = new BinaryOperationNode(new ConstantNode<Long>(INT, 3L),
+                                                             BinaryOperationNodeType.TIMES,
+                                                             new ConstantNode<Long>(INT, 8L));
+        final ASTBaseNode expectedAST = new BinaryOperationNode(new ConstantNode<Long>(INT, 5L),
+                                                                BinaryOperationNodeType.PLUS,
+                                                                multTree);
+
+        final ASTBaseNode actualAST = parser.parseExpression();
+
+        assertEquals(expectedAST, actualAST);
     }
 }
