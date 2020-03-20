0d957618a61ae4fb91dd607f6a265cd365aac27e
Mark Umnus <merkrafter@gmail.com>
2020-03-21 00:26:34 +0100

    Add Position data class

---
+++ src/main/java/com/merkrafter/lexing/Position.kt
@@ -0,0 +1,6 @@
+package com.merkrafter.lexing
+
+/**
+ * This class holds data about the position of an compiler-relevant object as Tokens, Nodes, etc.
+ */
+data class Position(val filename: String, val line: Long, val column: Int)
