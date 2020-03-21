d85dfd22204cdc5d0e0c4a7f582456ab21d988bd
Mark Umnus <merkrafter@gmail.com>
2020-03-21 02:47:19 +0100

    Remove unnecessary semicolons in IdentNode

--- src/main/java/com/merkrafter/representation/ast/IdentNode.kt
+++ src/main/java/com/merkrafter/representation/ast/IdentNode.kt
@@ -2,13 +2,13 @@ package com.merkrafter.representation.ast
 
 import com.merkrafter.lexing.Position
 import org.jetbrains.annotations.NotNull
-import java.util.LinkedList;
+import java.util.LinkedList
 
 data class IdentNode(val identifier: String, val position: Position) : AbstractSyntaxTree {
     /**
      * @return an empty list
      */
     override fun getAllErrors(): @NotNull MutableList<String> {
-        return LinkedList<String>();
+        return LinkedList<String>()
     }
 }
