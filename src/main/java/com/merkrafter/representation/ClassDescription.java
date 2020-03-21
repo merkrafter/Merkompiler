ff0a940cecc939afbca5c0b114ffc2bf5183199e
Mark Umnus <merkrafter@gmail.com>
2020-03-22 00:15:28 +0100

    Remove unused import in ClassDescription

--- src/main/java/com/merkrafter/representation/ClassDescription.java
+++ src/main/java/com/merkrafter/representation/ClassDescription.java
@@ -1,6 +1,5 @@
 package com.merkrafter.representation;
 
-import com.merkrafter.representation.ast.ProcedureCallNode;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
