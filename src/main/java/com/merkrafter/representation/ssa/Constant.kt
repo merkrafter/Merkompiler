2e6b59a2c0b841576afbf8070884be71ac9c1ae6
Mark Umnus <merkrafter@gmail.com>
2020-03-22 18:55:21 +0100

    Add Constant operands

---
+++ src/main/java/com/merkrafter/representation/ssa/Constant.kt
@@ -0,0 +1,18 @@
+package com.merkrafter.representation.ssa
+
+/****
+ * This class stores a reference to a constant integer value (as there are only integers in JavaSST
+ * so far).
+ *
+ * @since v0.5.0
+ * @author merkrafter
+ ***************************************************************/
+data class Constant(val value: Long) : Operand {
+    // METHODS
+    //==============================================================
+    // public methods
+    //--------------------------------------------------------------
+    override fun toString(): String {
+        return value.toString()
+    }
+}
