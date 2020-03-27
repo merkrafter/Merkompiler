fdbb2b65f0a7b2b6cb35f26be3c30c7025864a56
Mark Umnus <merkrafter@gmail.com>
2020-03-27 21:29:15 +0100

    Add SSATransformableClass interface

---
+++ src/main/java/com/merkrafter/representation/ssa/SSATransformableClass.java
@@ -0,0 +1,13 @@
+package com.merkrafter.representation.ssa;
+
+/****
+ * @since v0.5.0
+ * @author merkrafter
+ ***************************************************************/
+public interface SSATransformableClass {
+    // METHODS
+    //==============================================================
+    // public methods
+    //--------------------------------------------------------------
+    void transformToSSA();
+}
